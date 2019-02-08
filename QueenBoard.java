f830f66bcfcf4ee62152579c69ac6af025ff5aba
Samson Badlia <sbadlia00@stuy.edu>
2019-02-08 13:59:52 -0500

    Finished adding and removing queens as well as the board all tested

--- QueenBoard.java
+++ QueenBoard.java
@@ -28,22 +28,22 @@ public class QueenBoard{
 
   public boolean removeQueen(int r, int c){
     // checks if queen is in that section
-    if (board[r][c] != 0 ) return false;
-    //loops through board and removes a threat "-1" to the columns and rows and diagonals
-    for (int i = 0; i < board.length; i++ ){
-      board[r][i] = board[r][i] - 1;
-      board[i][c] = board[i][c] - 1;
-      // bottom left diagonal
-      if (r - i < board.length && r - i > 0 && c - i < board.length && c - i > 0) board[r - i][c - i] = board[r - i][c - i] + 1;
-      // upper right diagonal
-      if (r + i < board.length && c + i < board.length ) board[r + i][c + i] = board[r + i][c + i] + 1;
-      // upper left diagonal
-      if (r - i < board.length && r - i > 0 && c + i < board.length) board[r - i][c + i] = board[r - i][c + i] + 1;
-      // bottom right diagonal
-      if (r + i < board.length && c - i < board.length && c - i > 0) board[r + i][c - i] = board[r + i][c - i] + 1;
-    }
-    board[r][c] = 0;
-    return true;
+    if (board[r][c] != -1 ) return false;
+      //loops through board and removes a threat "-1" to the columns and rows and diagonals
+      for (int i = 0; i < board.length; i++ ){
+        board[r][i] = board[r][i] - 1;
+        board[i][c] = board[i][c] - 1;
+        // bottom left diagonal
+        if (r - i < board.length && r - i > 0 && c - i < board.length && c - i > 0) board[r - i][c - i] = board[r - i][c - i] - 1;
+        // upper right diagonal
+        if (r + i < board.length && c + i < board.length ) board[r + i][c + i] = board[r + i][c + i] - 1;
+        // upper left diagonal
+        if (r - i < board.length && r - i > 0 && c + i < board.length) board[r - i][c + i] = board[r - i][c + i] - 1;
+        // bottom right diagonal
+        if (r + i < board.length && c - i < board.length && c - i > 0) board[r + i][c - i] = board[r + i][c - i] - 1;
+      }
+      board[r][c] = 0;
+      return true;
   }
 
   public String toString(){
@@ -51,9 +51,9 @@ public class QueenBoard{
     for (int i = 0; i < board.length; i++){
       for (int j = 0; j < board[0].length; j++){
         //if queen present replaces with q
-        if (board[i][j] == -1) s+= "Q";
+        if (board[i][j] == -1) s+= " Q ";
         else{
-          s += "_";
+          s += " _ ";
         }
         if (j == board.length - 1) s+= "\n";
       }
@@ -65,7 +65,7 @@ public class QueenBoard{
     String s = "";
     for (int i = 0; i < board.length; i++){
       for (int j = 0; j < board[0].length; j++){
-        s += board[i][j];
+        s += board[i][j] + " ";
         if (j == board.length - 1) s+= "\n";
       }
     }
