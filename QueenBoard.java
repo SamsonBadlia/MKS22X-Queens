8915e088711f062dbb31a448f689b24f315a65db
Samson Badlia <sbadlia00@stuy.edu>
2019-02-08 14:05:45 -0500

    Added skeleton cases for solve and countSolutions

--- QueenBoard.java
+++ QueenBoard.java
@@ -72,4 +72,21 @@ public class QueenBoard{
     return s;
   }
 
+    /**
+  *@return false when the board is not solveable and leaves the board filled with zeros;
+  *        true when the board is solveable, and leaves the board in a solved state
+  *@throws IllegalStateException when the board starts with any non-zero value
+  */
+  public boolean solve(){
+
+  }
+
+  /**
+  *@return the number of solutions found, and leaves the board filled with only 0's
+  *@throws IllegalStateException when the board starts with any non-zero value
+  */
+  public int countSolutions(){
+    
+  }
+
 }
